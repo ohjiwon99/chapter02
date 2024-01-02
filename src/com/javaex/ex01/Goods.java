cd915351b7d748ea1bc751ef88415033a7874d63
ojiwon99 <dhwldnjs0420@naver.com>
2024-01-02 16:22:30 +0900

    2024-01-02 객체지향

---
+++ src/com/javaex/ex01/Goods.java
@@ -0,0 +1,25 @@
+package com.javaex.ex01;
+
+//해더
+public class Goods {
+	
+	
+	//필드
+	
+	
+	
+	String name;  //이름
+	int price;    //가격
+	
+	//생성자
+	//메소드-g/s
+	//    -일반
+
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/com/javaex/ex02/Goods.java
@@ -0,0 +1,13 @@
+package com.javaex.ex02;
+
+public class Goods {
+	    //필드
+
+		String name;  //이름
+		int price;    //가격
+		
+		//생성자
+		//메소드-g/s
+		//    -일반
+
+}
